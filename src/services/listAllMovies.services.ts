3b4384539136052314d3db031365036595539e2d
Eduardo Henrique <eduardohairescampos@gmail.com>
2023-04-27 20:11:27 -0300

    fix: remove_console.log_and_spaced_lines

--- src/services/listAllMovies.services.ts
+++ src/services/listAllMovies.services.ts
@@ -13,9 +13,6 @@ export const listAllMoviesService = async (
   const movieRepository: Repository<Movie> = AppDataSource.getRepository(Movie);
   let movies: Movie[] | undefined;
 
-  // console.log("page antes do if: ", page);
-  // console.log("perPage antes do if: ", perPage);
-
   if (!page || page < 0) {
     page = 1;
   }
@@ -24,16 +21,12 @@ export const listAllMoviesService = async (
     perPage = 5;
   }
 
-  // console.log("perPage depois do if: ", perPage);
-  // console.log("page depois do if: ", page);
-
   if (order !== "asc" && order !== "desc" || sort === undefined) {
     order = "asc";
   }
 
   if (sort !== "price" && sort !== "duration") {
-    console.log(sort)
-    console.log(order)
+
     sort = "id";
   }
 
@@ -61,12 +54,8 @@ export const listAllMoviesService = async (
   }
 
   if (page < Math.ceil(totalCount / perPage)) {
-    console.log(Math.ceil(totalCount / perPage))
     nextPage = `http://localhost:3000/movies?page=${page + 1}&perPage=${perPage}`;
   }
-
-
-
 
   return {
     prevPage,
